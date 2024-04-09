198e9e6171511744629366d72076fd596894daa1
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-04-09 18:12:36 +0100

    Update comments
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

--- src/main/java/com/github/lewisevans2007/LogColour.java
+++ src/main/java/com/github/lewisevans2007/LogColour.java
@@ -1,6 +1,7 @@
-/** Ping watch
+/* Ping watch
  * A java tool to monitor ping time with coloured terminal output.
  * GitHub: https://www.github.com/lewisevans2007/PingWatch
+ * Licence: GNU General Public License v3.0
  * By: Lewis Evans
  */
 
